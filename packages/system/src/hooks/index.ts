daa0a89bc08a29e6552d80e308a426c60ad6151e
Lexus Drumgold <lex@flexdevelopment.llc>
2020-10-05 20:52:13 -0400

    👌🏾 IMPROVE: remove unused components + hooks

--- packages/system/src/hooks/index.ts
+++ packages/system/src/hooks/index.ts
@@ -3,10 +3,10 @@
  * @module hooks
  */
 
-export * from './useContainer'
 export * from './useCustomAttributes'
 export * from './useIcon'
 export * from './useLineItemInput'
 export * from './useMutatedProps'
 export * from './useProductVariants'
 export * from './useQuantity'
+

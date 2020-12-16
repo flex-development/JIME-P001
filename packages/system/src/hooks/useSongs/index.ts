86d7a51c2f98bfb334569b2ef7089a9a6f6d50b3
Lexus Drumgold <lex@flexdevelopment.llc>
2020-12-16 01:45:54 -0500

    feat(kustomzdesign): useSongs

---
+++ packages/system/src/hooks/useSongs/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @file Hook - useSongs
+ * @module hooks/useSongs
+ */
+
+export * from './useSongs'
+
+/* eslint-disable prettier/prettier */
---
+++ packages/system/src/hooks/useSongs/useSongs.ts
@@ -0,0 +1,81 @@
+import { AnyObject } from '@flex-development/json'
+import { MusicKitSongAttributes as Song } from '@flex-development/kustomzcore'
+import { UseActiveIndex, useActiveIndex } from '@system/hooks/useActiveIndex'
+import { useMemoCompare } from '@system/hooks/useMemoCompare'
+import { clamp } from 'lodash'
+import { useCallback } from 'react'
+
+/**
+ * @file Use array of Apple Music song attributes
+ * @module hooks/useSongs/impl
+ */
+
+export type UseSongs = {
+  active: UseActiveIndex['active']
+  isActive: UseActiveIndex['isActive']
+  next: () => void
+  previous: () => void
+  song: Song | AnyObject
+}
+
+/**
+ * Returns an array of songs and functions to set the active song index.
+ *
+ * @param songs - Array of song attributes
+ * @param curr - Current song index
+ */
+export const useSongs = (songs: Song[] = [], curr = 0): UseSongs => {
+  // Handle internal songs state
+  const _songs = useMemoCompare<Song[]>(songs)
+
+  /**
+   * Clamps {@param index} between 0 and the number of the songs in the songs
+   * state array.
+   *
+   * @param index - New active song index
+   */
+  const clampIndex = (index: number) => clamp(index, 0, _songs.length - 1)
+
+  /* Callback version of `clampIndex` */
+  const clampIndexCB = useCallback(clampIndex, [_songs])
+
+  // Handle active song index
+  const active = useActiveIndex(clampIndexCB(curr))
+
+  // Handle current song state
+  const song = useMemoCompare<Song | AnyObject>(_songs[active.active] || {})
+
+  /**
+   * Increases the active index by 1.
+   */
+  const next = () => {
+    let new_active = active.active + 1
+    new_active = new_active > _songs.length - 1 ? 0 : new_active
+
+    return active.setIndex(clampIndexCB(new_active))
+  }
+
+  /* Callback version of `next` */
+  const nextCB = useCallback(next, [_songs, active, clampIndexCB])
+
+  /**
+   * Reduces the active index by 1.
+   */
+  const previous = () => {
+    let new_active = active.active - 1
+    new_active = new_active < 0 ? _songs.length - 1 : new_active
+
+    return active.setIndex(clampIndexCB(new_active))
+  }
+
+  /* Callback version of `previous` */
+  const previousCB = useCallback(previous, [_songs, active, clampIndexCB])
+
+  return {
+    active: active.active,
+    isActive: active.isActive,
+    next: nextCB,
+    previous: previousCB,
+    song
+  }
+}

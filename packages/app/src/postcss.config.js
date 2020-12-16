7835d93f976dd1898f33f7ccddee29a202eb8203
Lexus Drumgold <lex@flexdevelopment.llc>
2020-12-16 16:05:31 -0500

    chore(morenaskustomz): add custom postcss config

---
+++ packages/app/src/postcss.config.js
@@ -0,0 +1,8 @@
+const config = require('@flex-development/kustomzdesign/.postcssrc.json')
+
+/**
+ * @file PostCSS Configuration
+ * @see https://nextjs.org/docs/advanced-features/customizing-postcss-config
+ */
+
+module.exports = config

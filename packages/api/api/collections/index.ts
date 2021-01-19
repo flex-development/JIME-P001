40432a12e22f2c9f4110fb76dff2e066cc3ecc14
Lexus Drumgold <lex@flexdevelopment.llc>
2021-01-19 11:12:03 -0500

    feat(kapi): simple fields query logic for end users

--- packages/api/api/collections/index.ts
+++ packages/api/api/collections/index.ts
@@ -17,10 +17,7 @@ import type { FindCollectionsReq as Req } from '../../lib/types'
 import {
   collectionMetafields,
   collectionSEO,
-  findCollectionsOptions,
-  includeMetafields,
-  includeProducts,
-  includeSEO
+  findCollectionsOptions
 } from '../../lib/utils'
 
 /**
@@ -28,70 +25,67 @@ import {
  * @module api/collections
  */
 
+const COLLECTIONS = SHOPIFY.collectionListing
+
 export default async ({ query }: Req, res: Res): Promise<Res> => {
   // Convert collection query into search options object
   const options = findCollectionsOptions(query)
 
   try {
+    // Get collection listings to update search index
+    let listings: Hit[] | Promise<Hit>[] = await COLLECTIONS.list()
+
+    // Keep objectID consistent with collection listing ID
+    listings = listings.map(obj => ({ ...obj, objectID: obj.collection_id }))
+
+    // Get metafields for each collection
+    listings = listings.map(async hit => ({
+      ...hit,
+      metafield: await collectionMetafields(hit.collection_id)
+    }))
+
+    // Complete metafields promise
+    listings = await Promise.all(listings)
+
+    // Get products for each collection
+    listings = listings.map(async hit => {
+      const { product_listings } = await axiosShopify<SAR.ProductListing>({
+        method: 'get',
+        params: { collection_id: hit.collection_id, limit: 250 },
+        url: 'product_listings'
+      })
+
+      return { ...hit, products: product_listings }
+    })
+
+    // Complete products promise
+    listings = await Promise.all(listings)
+
+    // Get SEO data for each collection
+    listings = listings.map(async hit => {
+      const collection = (hit as unknown) as Hit
+      const products = (hit as AnyObject).products
+
+      return { ...hit, seo: await collectionSEO(collection, products) }
+    })
+
+    // Complete SEO data promise
+    listings = await Promise.all(listings)
+
     // Initialize search index
     const index = ALGOLIA.initIndex(INDEX_SETTINGS.collection_listings.name)
 
     // Set index settings
     index.setSettings(omit(INDEX_SETTINGS.collection_listings, ['name']))
 
-    // Get collection listings to update index
-    let listings = await SHOPIFY.collectionListing.list()
-
-    // Keep objectID consistent with collection listing ID
-    listings = listings.map(obj => ({ ...obj, objectID: obj.collection_id }))
-
     // Update index data
     await index.saveObjects(listings)
 
     // Perform search
     const { hits } = await index.search<Hit>(query?.text ?? '', options)
-    let payload: Hit[] | Promise<Hit>[] = hits
 
-    // Because product data is required to fetch SEO data, check for either
-    if (includeProducts(options) || includeSEO(options)) {
-      // Get products for each collection
-      payload = payload.map(async hit => {
-        const { product_listings } = await axiosShopify<SAR.ProductListing>({
-          method: 'get',
-          params: { collection_id: hit.collection_id, limit: 250 },
-          url: 'product_listings'
-        })
-
-        return { ...hit, products: product_listings }
-      })
-
-      // Complete products promise
-      payload = await Promise.all(payload)
-
-      // Get SEO data for each collection
-      if (includeSEO(options)) {
-        payload = payload.map(async hit => {
-          const collection = (hit as unknown) as Hit
-          const products = (hit as AnyObject).products
-
-          return { ...hit, seo: await collectionSEO(collection, products) }
-        })
-
-        payload = await Promise.all(payload)
-      }
-    }
-
-    // Get metafields for each collection
-    if (includeMetafields(options)) {
-      payload = payload.map(async hit => ({
-        ...hit,
-        metafield: await collectionMetafields(hit.collection_id)
-      }))
-
-      payload = await Promise.all(payload)
-    }
-
-    return res.json(payload.map(data => omit(data, ['objectID'])))
+    // Return results and remove `objectID` field
+    return res.json(hits.map(data => omit(data, ['objectID'])))
   } catch (err) {
     const error = createError(err, { options, query }, err.status)
 
--- packages/api/api/products/index.ts
+++ packages/api/api/products/index.ts
@@ -1,10 +1,8 @@
-import {
-  createError,
-  IProductListing as Hit
-} from '@flex-development/kustomzcore'
+import { createError, IProductListing } from '@flex-development/kustomzcore'
 import { VercelResponse as Res } from '@vercel/node'
 import debug from 'debug'
 import omit from 'lodash/omit'
+import type { IProductListing as Hit } from 'shopify-api-node'
 import { ALGOLIA, INDEX_SETTINGS, SHOPIFY } from '../../lib/config'
 import type {
   FindCollectionsReq as Req,
@@ -12,8 +10,6 @@ import type {
 } from '../../lib/types'
 import {
   findProductsOptions,
-  includeMetafields,
-  includeSEO,
   productMetafields,
   productSEO
 } from '../../lib/utils'
@@ -23,51 +19,51 @@ import {
  * @module api/products
  */
 
+const PRODUCTS = SHOPIFY.productListing
+
 export default async ({ query }: Req, res: Res): Promise<Res> => {
   // Convert product query into search options object
   const options = findProductsOptions(query)
 
   try {
+    // Get product listings to update search index
+    let listings: Hit[] | Promise<Hit>[] = await PRODUCTS.list()
+
+    // Keep objectID consistent with product listing ID
+    listings = listings.map(obj => ({ ...obj, objectID: obj.product_id }))
+
+    // Get metafields for each product
+    listings = listings.map(async hit => ({
+      ...hit,
+      metafield: await productMetafields(hit.product_id)
+    }))
+
+    // Complete metafields promise
+    listings = await Promise.all(listings)
+
+    // Get SEO data for each product
+    listings = listings.map(async hit => ({
+      ...hit,
+      seo: productSEO(hit as IProductListing, (query as GetProductQuery).sku)
+    }))
+
+    // Complete SEO data promise
+    listings = await Promise.all(listings)
+
     // Initialize search index
     const index = ALGOLIA.initIndex(INDEX_SETTINGS.product_listings.name)
 
     // Set index settings
     index.setSettings(omit(INDEX_SETTINGS.product_listings, ['name']))
 
-    // Get product listings to update index
-    let listings = await SHOPIFY.productListing.list()
-
-    // Keep objectID consistent with product listing ID
-    listings = listings.map(obj => ({ ...obj, objectID: obj.product_id }))
-
     // Update index data
     await index.saveObjects(listings)
 
     // Perform search
     const { hits } = await index.search<Hit>(query?.text ?? '', options)
-    let payload: Hit[] | Promise<Hit>[] = hits
 
-    // Get metafields for each product
-    if (includeMetafields(options)) {
-      payload = payload.map(async hit => ({
-        ...hit,
-        metafield: await productMetafields(hit.product_id)
-      }))
-
-      payload = await Promise.all(payload)
-    }
-
-    // Get SEO data for each product
-    if (includeSEO(options)) {
-      payload = payload.map(async hit => ({
-        ...hit,
-        seo: productSEO(hit, (query as GetProductQuery).sku)
-      }))
-
-      payload = await Promise.all(payload)
-    }
-
-    return res.json(payload.map(data => omit(data, ['objectID'])))
+    // Return results and remove `objectID` field
+    return res.json(hits.map(data => omit(data, ['objectID'])))
   } catch (err) {
     const error = createError(err, { options, query }, err.status)
 

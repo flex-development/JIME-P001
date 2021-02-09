ad7b0e3ab009d3245234781b0439c6e993397eb6
Lexus Drumgold <lex@flexdevelopment.llc>
2021-02-09 12:08:13 -0500

    fix(kapi): apple music 401 in dev env
    fix(kapi): apple music 401 in dev env

--- packages/api/lib/utils/appleDeveloperToken.ts
+++ packages/api/lib/utils/appleDeveloperToken.ts
@@ -1,5 +1,6 @@
-import createError from '@flex-development/kustomzcore/dist/utils/createError'
-import jwt, { Algorithm, SignOptions } from 'jsonwebtoken'
+import { createError } from '@flex-development/kustomzcore'
+import type { Algorithm, SignOptions } from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 import isEmpty from 'lodash/isEmpty'
 
 /**
@@ -7,46 +8,40 @@ import isEmpty from 'lodash/isEmpty'
  * @module lib/utils/appleDeveloperToken
  */
 
+const {
+  APPLE_AUTHKEY_MUSICKIT: private_key,
+  APPLE_AUTHKEY_MUSICKIT_KEY_ID: kid,
+  APPLE_TEAM_ID: issuer
+} = process.env
+
 /**
  * Returns a signed JSON web token to authenticate with the Apple Music API.
  *
  * @throws {FeathersErrorJSON}
  */
 const appleDeveloperToken = (): string => {
+  // Throw errors if missing Apple Music API credentials
+  if (isEmpty(private_key)) throw createError('Missing APPLE_AUTHKEY_MUSICKIT')
+  if (isEmpty(kid)) throw createError('Missing APPLE_AUTHKEY_MUSICKIT_KEY_ID')
+  if (isEmpty(issuer)) throw createError('Missing APPLE_TEAM_ID')
+
+  /**
+   * NOTICE: Apple Music supports only developer tokens that are signed with the
+   * ES256 algorithm. Unsecured JWTs, or JWTs signed with other algorithms, are
+   * rejected and result in a `403` error code.
+   */
   const algorithm: Algorithm = 'ES256'
 
-  const payload = {
-    exp: 15777000,
-    iat: new Date().valueOf(),
-    iss: process.env.APPLE_TEAM_ID || ''
-  }
-
-  if (isEmpty(payload.iss)) {
-    throw createError('Missing APPLE_TEAM_ID', { payload })
-  }
-
-  const options = {
+  // Token signing options
+  const options: SignOptions = {
     algorithm,
-    expiresIn: payload.exp,
-    header: {
-      alg: algorithm,
-      kid: process.env.APPLE_AUTHKEY_MUSICKIT_KEY_ID || ''
-    },
-    issuer: payload.iss
+    expiresIn: '180d',
+    header: { alg: algorithm, kid },
+    issuer
   }
 
-  if (isEmpty(options.header.kid)) {
-    throw createError('Missing APPLE_AUTHKEY_MUSICKIT_KEY_ID', { options })
-  }
-
-  let private_key = process.env.APPLE_AUTHKEY_MUSICKIT?.replace(/\\n/g, '\n')
-  private_key = private_key || ''
-
-  if (isEmpty(private_key)) {
-    throw createError('Missing APPLE_AUTHKEY_MUSICKIT', { options, payload })
-  }
-
-  return jwt.sign({}, private_key, options as SignOptions)
+  // Return signed token
+  return jwt.sign({}, private_key?.replace(/\\n/g, '\n') ?? '', options)
 }
 
 export default appleDeveloperToken

b03404ebfa265bc5ce2f2c19e13f8806bd3a226e
Lexus Drumgold <lex@flexdevelopment.llc>
2021-02-08 23:47:36 -0500

    fix(kapi): utils_1.includeSEO is not a function

--- packages/api/api/products/index.ts
+++ packages/api/api/products/index.ts
@@ -1,4 +1,3 @@
-import type { IProductListing } from '@flex-development/kustomzcore'
 import type { VercelResponse as Res } from '@vercel/node'
 import {
   handleAPIError,
@@ -7,8 +6,7 @@ import {
   trackAPIRequest
 } from '../../lib/middleware'
 import Service from '../../lib/services/ProductService'
-import type { FindProductsReq as Req, GetProductQuery } from '../../lib/types'
-import { includeSEO } from '../../lib/utils'
+import type { FindProductsReq as Req } from '../../lib/types'
 
 /**
  * @file API Endpoint - Find Products
@@ -41,23 +39,7 @@ export default async (req: Req, res: Res): Promise<Res | void> => {
   const options = Service.searchOptions(req.query)
 
   try {
-    // Execute search and get results
-    let results = await Service.find(req.query.text, options)
-
-    // Get SEO data for each product
-    if (includeSEO(options)) {
-      const $results = results.map(async obj => {
-        const product = obj as IProductListing
-        const { sku } = req.query as GetProductQuery
-
-        return { ...obj, seo: await Service.seo(product, sku) }
-      })
-
-      // Complete SEO data promise
-      results = await Promise.all($results)
-    }
-
-    res.json(results)
+    res.json(await Service.find(req.query.text, options))
   } catch (err) {
     return handleAPIError(req, res, err, { query: req.query })
   }

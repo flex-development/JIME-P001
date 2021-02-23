8ca4d9115fd6a3f11d718ed81e4c9e1267ed5374
Lexus Drumgold <lex@flexdevelopment.llc>
2021-02-23 10:23:53 -0500

    chore(kapi): serialize vercel env obj

--- packages/api/lib/config/vercel-env.ts
+++ packages/api/lib/config/vercel-env.ts
@@ -1,10 +1,12 @@
+import { serialize } from '@flex-development/json'
+
 /**
  * @file Common Vercel System Environment Variables
  * @module lib/config/vercel-env
  */
 
-export default {
+export default serialize({
   branch: process.env.VERCEL_GIT_COMMIT_REF || '',
   commit: process.env.VERCEL_GIT_COMMIT_SHA || '',
   env: process.env.VERCEL_ENV || ''
-}
+})

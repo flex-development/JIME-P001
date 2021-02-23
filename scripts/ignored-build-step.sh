284ea83f2c9f38d8a1427e6d73c294a0cd02bf9f
Lexus Drumgold <lex@flexdevelopment.llc>
2021-02-23 10:00:42 -0500

    build: update ignored-build-step workflow

--- scripts/ignored-build-step.sh
+++ scripts/ignored-build-step.sh
@@ -5,21 +5,26 @@
 # If a code "1" or greater is returned, then a new deployment will be built.
 # See: https://vercel.com/docs/platform/projects#ignored-build-step
 
+# Get number of commits to check for initial commit
+HEAD_COMMIT_COUNT=$(git rev-list --count HEAD)
+
 # Common files to check for changes in the root of each package
-COMMON_FILES="package.json tsconfig.json vercel.json"
+COMMON="*.json"
 
 # Check for changes in `packages/core`
-CORE_PACKAGE="../core/src ../core/package.json ../core/tsconfig.json ../core/tsconfig.prod.json"
+CORE="../core/src ../core/package.json ../core/tsconfig.*"
 
 # Check for changes in root package
-ROOT_PACKAGE="../../scripts ../../.vercelignore ../../babel.config.json ../../lerna.json ../../package.json ../../tsconfig.json ../../yarn.lock"
+ROOT="../../scripts ../../.vercelignore ../../*.json ../../yarn.lock"
 
-if [[ "$@" == "api" ]]; then
-  git diff HEAD^ HEAD --quiet api lib $COMMON_FILES $CORE_PACKAGE $ROOT_PACKAGE
+if [[ $HEAD_COMMIT_COUNT == 1 ]]; then
+  exit 1
+elif [[ "$@" == "api" ]]; then
+  git diff HEAD^ HEAD --quiet api lib $COMMON $CORE $ROOT
 elif [[ "$@" == "app" ]]; then
-  git diff HEAD^ HEAD --quiet public scripts src babel.config.js next.config.js postcss.config.js robots-txt.config.js tsconfig.dev.json tsconfig.prod.json ../api/lib/types.ts ../system/src/**/* ../system/src/index.ts ../system/package.json ../system/tsconfig.prod.json $COMMON_FILES $CORE_PACKAGE $ROOT_PACKAGE
+  git diff HEAD^ HEAD --quiet public scripts src babel.config.js next.config.js postcss.config.js robots-txt.config.js ../system/src/**/* ../system/src/index.ts ../system/package.json ../system/tsconfig.* ../api/lib/types.ts $COMMON $CORE $ROOT
 elif [[ "$@" == "system" ]]; then
-  git diff HEAD^ HEAD --quiet .storybook src babel.config.js postcss.config.js tsconfig.app.json webpack.common.js $COMMON_FILES $CORE_PACKAGE $ROOT_PACKAGE
+  git diff HEAD^ HEAD --quiet .storybook scripts src postcss.config.js webpack.* $COMMON $CORE $ROOT
 else
-  git diff HEAD^ HEAD --quiet $COMMON_FILES $ROOT_PACKAGE
+  git diff HEAD^ HEAD --quiet $COMMON $ROOT
 fi

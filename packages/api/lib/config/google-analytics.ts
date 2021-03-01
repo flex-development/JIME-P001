b21e2ae6999d077ec326fcb1cc94f709b534f654
Lexus Drumgold <lex@flexdevelopment.llc>
2021-03-01 10:10:28 -0500

    chore(kapi): disable ga in non-vercel envs

--- packages/api/lib/config/google-analytics.ts
+++ packages/api/lib/config/google-analytics.ts
@@ -8,10 +8,15 @@ import { nanoid } from 'nanoid'
  * @see https://github.com/wusuopu/ts-ga-measurement-protocol
  */
 
-// Initialize Measure Protocal client
-const ga = new GA(process.env.GA_TRACKING_ID as string, Axios, '1', true)
+const { GA_TRACKING_ID, VERCEL, VERCEL_ENV } = process.env
+
+// Initialize Measure Protocol client
+const ga = new GA(GA_TRACKING_ID || '', Axios, '1', true)
 
 // Identifies a particular user, device, or browser instance
 ga.setClientId(nanoid())
+
+// Disable tracking in non-Vercel environments
+if (!(JSON.parse(VERCEL || '0') && VERCEL_ENV !== 'development')) ga.disable()
 
 export default ga

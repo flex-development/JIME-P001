4a05a47c2e9ce115b41e7670debe2d22fa6deb3c
Lexus Drumgold <lex@flexdevelopment.llc>
2021-03-02 19:16:32 -0500

    chore(morenaskustomz): log __dirname,  proces.cwd
    original css dir doesnt exist

--- packages/app/src/components/InlineStylesHead/InlineStylesHead.tsx
+++ packages/app/src/components/InlineStylesHead/InlineStylesHead.tsx
@@ -37,7 +37,7 @@ export class InlineStylesHead extends Head {
     const dir = `.next/server${process.env.VERCEL ? 'less' : ''}/static/css`
     const resdir = resolve(process.cwd(), dir)
 
-    console.debug({ cwd: process.cwd() })
+    console.debug({ __dirname, cwd: process.cwd() })
 
     if (!existsSync(resdir)) return []
 
